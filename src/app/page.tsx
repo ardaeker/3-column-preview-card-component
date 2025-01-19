617ba6b3b7726684b72c302d141e804c3a7958b2
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-01-20 00:23:42 +0300

    fix: ensure Page includes a level-one heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Categories } from '@/components/categories';
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center px-6 max-lg:py-[5.5rem] lg:pt-[2.625rem]">
+      <h1 className="sr-only">3 Column Preview Card Component</h1>
       <Categories />
     </main>
   );
